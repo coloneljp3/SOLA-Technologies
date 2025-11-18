1139b2cbda596087e05007771a802db431f445cb
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-11-18 02:49:13 -0600

    Update create_account.js

--- functions/create_account.js
+++ functions/create_account.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use('/',(req,res)=>{
 var user = req.body.username
 var psw = req.body.psw
-var conn = mysql.createConnection(process.env.DATABASE_URL)
+var conn = mysql.createConnection("mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED")
 conn.query(`Select COUNT(*) FROM Accounts WHERE username = ? AND pasword = ?`,[user,psw],(err,results,fields) => {
     res.send(err); 
 
