efe8105f4a562ebadb1a662b9c5984e0e989d4d1
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-12-02 22:17:27 -0600

    Create create_table.js

---
+++ functions/create_table.js
@@ -0,0 +1,26 @@
+'use strict'
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+const serverless = require('serverless-http');
+const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/',(req,res)=>{
+var user = req.body.username
+var psw = req.body.psw
+var conn = mysql.createConnection("mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED")
+conn.query(`Create Table Accounts(
+ID int AUTO_INCREMENT NOT NULL,
+type_of_acct varchar(40),
+username varchar(40), 
+pasword varchar(40)
+);`,[user,psw],(err,results,fields) => {
+    res.send(err); 
+
+})})
+
+app.use('/.netlify/functions/create_table',router)
+
+module.exports.handler = serverless(app)
