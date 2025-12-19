6c8c52bc0e7a69126a4e15f8773748ce8f205371
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-12-19 03:07:10 -0600

    Update create_table.js

--- functions/create_table.js
+++ functions/create_table.js
@@ -13,10 +13,20 @@ var psw = req.body.psw
 var conn = mysql.createConnection("mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED")
 conn.query(`
 
-Create Table Contributions(
+Create Table Case_Studies(
 ID int AUTO_INCREMENT NOT NULL,
-account_name varchar(40),
-contributions TEXT,
+first_name varchar(40),
+last_name varchar(40),
+MI varchar(40),
+previous_acl_injuries varchar(40),
+previous_injuries_list varchar(255),
+sport varchar(30),
+athlete_bool varchar(10),
+diet varchar(50),
+weight varchar(10),
+height varchar(10),
+race varchar(40),
+foot_image LONGBLOB NOT NULL, 
 PRIMARY KEY (id)
 
 
