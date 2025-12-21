181c2ca49191dd4c37f3059b2252f176296d7fb7
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-12-21 15:59:02 -0600

    Update new_case.js

--- functions/new_case.js
+++ functions/new_case.js
@@ -9,28 +9,9 @@ const gcloud = require('@google-cloud/language')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.post('/',(req,res)=>{
-var first_name = req.body.first_name;
-var last_name = req.body.last_name;
-var MI = req.body["M.I."];
-var prev_injuries_list = req.body.prev_injuries_list;
-var sport = req.body.sport;
-var diet = req.body.diet;
-var height_in = req.body.height_in;
-var height_ft = req.body.height_ft;
-var race = req.body.race;
-var sex = req.body.sex;
-var foot_image = req.body.foot_image;
-var knee_img = req.body.knee_img;
-var citation = req.body.citation;
-
-var conn = mysql.createConnection("mysql://avnadmin:AVNS_om8uYVTBL50tPl05R_4@mysql-1e9f0822-jpbreaux225-37e4.h.aivencloud.com:25589/defaultdb?ssl-mode=REQUIRED")
-conn.query(`INSERT INTO Case_Studies(first_name, last_name, MI, previous_injuries_list, sport, diet, weight, height, race, foot_image, citation) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)`,[
-  first_name, last_name, MI, prev_injuries_list, sport, diet, height_ft + " "+height_in, race, foot_image, citation],(err,results)=>{
-  if(err){res.send("There was an error: " + err )}
-  res.send("Submitted");
-  })
-  
+  res.send('Yo')
 })
 
 app.use('/.netlify/functions/new_case',router);
+
 module.exports.hander = serverless(app);
